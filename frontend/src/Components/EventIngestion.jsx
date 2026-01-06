a941c784709a2a08351ec7288231309d9831f25f
[Harshit Gupta] <[hrstgpt87@gmail.com]>
2026-01-06 15:19:28 +0530

    Base Component Structure

---
+++ frontend/src/Components/EventIngestion.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const EventIngestion = () => {
+  return (
+    <div>
+        <button>Email Open</button>
+        <button>Page View</button>
+        <button>Form Submission</button>
+        <button>Demo request</button>
+        <button>Purchase</button>
+      
+    </div>
+  )
+}
+
+export default EventIngestion
---
+++ frontend/src/Components/RuleConfig.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const RuleConfig = () => {
+  return (
+    <div>
+        <form action="submit">
+            <label>Email Open Points <input type="text"  /></label>
+            <label>Page View Points <input type="text"  /></label>
+            <label>Form Submission Points <input type="text"  /></label>
+            <label>Demo Request Points <input type="text"  /></label>
+            <label>Purchase Points <input type="text"  /></label>
+        </form>
+      
+    </div>
+  )
+}
+
+export default RuleConfig
